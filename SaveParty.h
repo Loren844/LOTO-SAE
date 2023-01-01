5ead83566d783382872f0e384e7f6cfab31678be
Nelveee <lorenzo.turlettipro@gmail.com>
2023-01-01 15:05:23 +0100

    Add functions which save and restore a party in progress(only cardboard)

--- SaveParty.h
+++ SaveParty.h
@@ -4,7 +4,6 @@
 
 #ifndef LOTO_SAE_SAVEPARTY_H
 #define LOTO_SAE_SAVEPARTY_H
-
-#endif //LOTO_SAE_SAVEPARTY_H
 void saveGrille(Case grille[3][9]);
 void recupere_donnees_fichier(Case grille[3][9]);
+#endif //LOTO_SAE_SAVEPARTY_H
