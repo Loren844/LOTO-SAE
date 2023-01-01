1cdc093310a65466bbb2db52631709d3305c4700
Nelveee <lorenzo.turlettipro@gmail.com>
2023-01-01 15:01:48 +0100

    Add functions which save and restore a party in progress(only cardboard)

---
+++ SaveParty.c
@@ -0,0 +1,54 @@
+//
+// Created by loren on 01/01/2023.
+//
+
+#include "SaveParty.h"
+//Faire les imports necessaire dans le main.
+void saveGrille(Case grille[3][9])
+{
+    FILE* fichier = NULL;
+    fichier = fopen("stockStruc.txt","w");
+    if(fichier !=NULL)
+    {
+        for(int k = 0 ; k < 3 ; k++)
+        {
+            for(int l = 0 ; l < 9 ; l++)
+            {
+                fprintf(fichier,"%d %d;",grille[k][l].val,grille[k][l].estTiree);
+            }
+        }
+        fprintf(fichier,"\n");
+        fclose(fichier);
+    }
+}
+
+void recupere_donnees_fichier(Case grille[3][9])
+{
+    FILE* fichier = NULL;
+    int i=0,j=0,k=1;
+    int value = 0;
+    char ligne[256];
+    fichier = fopen("stockStruc.txt","r");
+    fgets(ligne, sizeof(ligne), fichier);
+    const char * separators = " ;";
+    char * strToken = strtok ( ligne, separators );
+    while(strToken!=NULL)
+    {
+        if(k%2==0)
+        {
+            (&grille[i][j])->val = value;
+            j++;
+            if(j%9==0)
+            {
+                i++;
+                j=0;
+            }
+        } else
+        {
+            (&grille[i][j])->estTiree = value;
+        }
+        value = atoi(strToken);
+        strToken = strtok ( NULL, separators );
+        k++;
+    }
+}
